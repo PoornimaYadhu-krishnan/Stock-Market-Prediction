e960c928066dfc02cb4ee56acaa74462d29d5476
PoornimaYadhu-krishnan <59013805+PoornimaYadhu-krishnan@users.noreply.github.com>
2021-06-08 19:41:17 +0530

    error-import

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import pandas as pd
 from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_style('whitegrid')
-%matplotlib inline
 from __future__ import division
 from datetime import datetime
 from nsepy import get_history
