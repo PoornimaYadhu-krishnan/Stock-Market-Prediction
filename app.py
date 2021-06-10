66dfdea1f260d7d09fb8457aa020848770d0fdb0
PoornimaYadhu-krishnan <59013805+PoornimaYadhu-krishnan@users.noreply.github.com>
2021-06-10 21:18:43 +0530

    test1

--- app.py
+++ app.py
@@ -31,13 +31,72 @@ def Search():
 def About():
     return render_template('About.html')
 
+def plotgraph():
+    end = datetime.now()
+    start = datetime(end.year-1,end.month,end.day)
+    stock_df = get_history(symbol="HDFC",start=start,end=end)
+    fig = plt.figure(figsize=(10,5))
+    plt.xlabel('Year-Month') 
+    plt.ylabel('Stock-Price(in Rupees.)') 
+    plt.title('Closing price of the stock for last 1 Year') 
+    stock_df['Close'].plot(legend=True, figsize=(10,4))
+    fig.savefig('static/stock-plot.jpg', bbox_inches='tight', dpi=150)
+
+def stock_monte_carlo(start_price,days,mu,sigma):
+    ''' This function takes in starting stock price, days of simulation,mu,sigma, and returns simulated price array'''
+    dt = 1/days
+    # Define a price array
+    price = np.zeros(days)
+    price[0] = start_price
+    
+    # Schok and Drift
+    shock = np.zeros(days)
+    drift = np.zeros(days)
+    
+    # Run price array for number of days
+    for x in range(1,days):
+        
+        # Calculate Schock
+        shock[x] = np.random.normal(loc=mu * dt, scale=sigma * np.sqrt(dt))
+        # Calculate Drift
+        drift[x] = mu * dt
+        # Calculate Price
+        price[x] = price[x-1] + (price[x-1] * (drift[x] + shock[x]))
+        
+    return price
+
 
 @app.route('/stock_pred/',methods = ['POST'])
 def stock_pred():
-    user_input = request.form['fn']
-
+    # user_input = request.form['fn']
     try:
-        return  render_template('Result.html')
+        end = datetime.now()
+        start = datetime(end.year-2,end.month,end.day)
+        l = list()
+        stock_df = get_history(symbol="HDFC",start=start,end=end)
+        closingprice_df = stock_df['Close']
+        closingprice_df=closingprice_df.to_frame()
+        tech_returns = closingprice_df.pct_change()
+        rets = tech_returns.dropna()
+        days = 180
+        mu = rets.mean()['Close']
+        sigma = rets.std()['Close']
+        start_price = closingprice_df.iloc[-1, closingprice_df.columns.get_loc("Close")]
+        runs = 100000
+        simulations = np.zeros(runs)
+        for run in range(runs):    
+            simulations[run] = stock_monte_carlo(start_price,days,mu,sigma)[days-1]
+        max_low = np.percentile(simulations,1)
+        num_stocks=10000/start_price
+        current_value=simulations.mean()*num_stocks
+        profit_percent=((current_value-10000)/10000)*100
+        max_loss=(start_price-max_low)*num_stocks
+        loss_percent=(max_loss/10000)*100
+        l.append(current_value)
+        l.append(profit_percent)
+        l.append(max_loss)
+        l.append(loss_percent)
+        return  render_template('Result.html',predictions=l)
     
 
     except:
